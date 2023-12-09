e1ea4e8427a18e3103ace5211e324d75320430c9
Daniel Vitor <danielsalviano879@gmail.com>
2023-12-09 19:34:57 -0300

    Alteração na tela de cadastro

--- SRC/aed-trabalho/Form1.Designer.cs
+++ SRC/aed-trabalho/Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace aed_trabalho
+﻿namespace cadastronovo
 {
-    partial class Início
+    partial class Form1
     {
         /// <summary>
         ///  Required designer variable.
@@ -30,191 +30,116 @@
         {
             label1 = new Label();
             label2 = new Label();
-            button1 = new Button();
-            txtSenha = new TextBox();
-            backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
-            aEDMarketToolStripMenuItem = new ToolStripMenuItem();
-            ds = new ToolStripMenuItem();
-            menuStrip1 = new MenuStrip();
-            txtUsuario = new TextBox();
-            groupBox1 = new GroupBox();
-            checkbox = new CheckBox();
-            button3 = new Button();
+            txtusuario = new TextBox();
+            txtsenha = new TextBox();
             label3 = new Label();
+            button1 = new Button();
             button2 = new Button();
-            menuStrip1.SuspendLayout();
-            groupBox1.SuspendLayout();
+            button3 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(47, 46);
+            label1.Location = new Point(67, 23);
             label1.Name = "label1";
-            label1.Size = new Size(127, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Nome do usuário:";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 0;
+            label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(46, 112);
+            label2.Location = new Point(89, 23);
             label2.Name = "label2";
-            label2.Size = new Size(52, 20);
-            label2.TabIndex = 2;
-            label2.Text = "Senha:";
+            label2.Size = new Size(59, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Usuário";
             label2.Click += label2_Click;
             // 
-            // button1
+            // txtusuario
             // 
-            button1.Location = new Point(109, 226);
-            button1.Name = "button1";
-            button1.Size = new Size(107, 29);
-            button1.TabIndex = 3;
-            button1.Text = "Cadastrar-se";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            txtusuario.Location = new Point(89, 62);
+            txtusuario.Name = "txtusuario";
+            txtusuario.Size = new Size(130, 27);
+            txtusuario.TabIndex = 2;
+            txtusuario.TextChanged += textBox1_TextChanged;
             // 
-            // txtSenha
+            // txtsenha
             // 
-            txtSenha.Location = new Point(46, 141);
-            txtSenha.Name = "txtSenha";
-            txtSenha.Size = new Size(125, 27);
-            txtSenha.TabIndex = 5;
-            txtSenha.UseSystemPasswordChar = true;
-            txtSenha.TextChanged += textBox2_TextChanged;
-            // 
-            // backgroundWorker2
-            // 
-            backgroundWorker2.DoWork += backgroundWorker2_DoWork;
-            // 
-            // aEDMarketToolStripMenuItem
-            // 
-            aEDMarketToolStripMenuItem.Name = "aEDMarketToolStripMenuItem";
-            aEDMarketToolStripMenuItem.Size = new Size(108, 24);
-            aEDMarketToolStripMenuItem.Text = "AED- Market";
-            // 
-            // ds
-            // 
-            ds.Name = "ds";
-            ds.Size = new Size(14, 24);
-            ds.Click += toolStripMenuItem2_Click;
-            // 
-            // menuStrip1
-            // 
-            menuStrip1.ImageScalingSize = new Size(20, 20);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { aEDMarketToolStripMenuItem, ds });
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(778, 28);
-            menuStrip1.TabIndex = 6;
-            menuStrip1.Text = "menuStrip1";
-            // 
-            // txtUsuario
-            // 
-            txtUsuario.Location = new Point(46, 82);
-            txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(125, 27);
-            txtUsuario.TabIndex = 4;
-            txtUsuario.TextChanged += textBox1_TextChanged;
-            // 
-            // groupBox1
-            // 
-            groupBox1.BackColor = SystemColors.ButtonFace;
-            groupBox1.Controls.Add(checkbox);
-            groupBox1.Controls.Add(button3);
-            groupBox1.Controls.Add(label3);
-            groupBox1.Controls.Add(button2);
-            groupBox1.Controls.Add(txtSenha);
-            groupBox1.Controls.Add(txtUsuario);
-            groupBox1.Controls.Add(button1);
-            groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(label1);
-            groupBox1.ForeColor = SystemColors.ActiveCaptionText;
-            groupBox1.Location = new Point(298, 67);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(231, 316);
-            groupBox1.TabIndex = 7;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "Formulário:";
-            groupBox1.Enter += groupBox1_Enter;
-            // 
-            // checkbox
-            // 
-            checkbox.AutoSize = true;
-            checkbox.Location = new Point(45, 187);
-            checkbox.Name = "checkbox";
-            checkbox.Size = new Size(126, 24);
-            checkbox.TabIndex = 10;
-            checkbox.Text = "Deixar logado";
-            checkbox.UseVisualStyleBackColor = true;
-            checkbox.CheckedChanged += checkBox2_CheckedChanged;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(118, 281);
-            button3.Name = "button3";
-            button3.Size = new Size(107, 29);
-            button3.TabIndex = 8;
-            button3.Text = "Logar";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
+            txtsenha.Location = new Point(89, 164);
+            txtsenha.Name = "txtsenha";
+            txtsenha.Size = new Size(130, 27);
+            txtsenha.TabIndex = 3;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(6, 285);
+            label3.Location = new Point(89, 115);
             label3.Name = "label3";
-            label3.Size = new Size(116, 20);
-            label3.TabIndex = 9;
-            label3.Text = "Já possui conta?";
-            label3.Click += label3_Click_1;
+            label3.Size = new Size(49, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Senha";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(157, 234);
+            button1.Name = "button1";
+            button1.Size = new Size(38, 14);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(6, 226);
+            button2.Location = new Point(44, 234);
             button2.Name = "button2";
-            button2.Size = new Size(97, 29);
-            button2.TabIndex = 8;
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 6;
             button2.Text = "Cancelar";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
-            // Início
+            // button3
+            // 
+            button3.Location = new Point(157, 234);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 7;
+            button3.Text = "Confirmar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Aquamarine;
-            ClientSize = new Size(778, 482);
-            Controls.Add(groupBox1);
-            Controls.Add(menuStrip1);
-            MainMenuStrip = menuStrip1;
-            Name = "Início";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Início";
+            ClientSize = new Size(301, 296);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(txtsenha);
+            Controls.Add(txtusuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Cadastro";
             Load += Form1_Load;
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
+
         private Label label1;
         private Label label2;
+        private TextBox txtusuario;
+        private TextBox txtsenha;
+        private Label label3;
         private Button button1;
-        private TextBox txtSenha;
-        private System.ComponentModel.BackgroundWorker backgroundWorker2;
-        private ToolStripMenuItem aEDMarketToolStripMenuItem;
-        private ToolStripMenuItem ds;
-        private MenuStrip menuStrip1;
-        private TextBox txtUsuario;
-        private GroupBox groupBox1;
         private Button button2;
         private Button button3;
-        private Label label3;
-        private CheckBox checkbox;
     }
 }
--- SRC/aed-trabalho/Form1.cs
+++ SRC/aed-trabalho/Form1.cs
@@ -1,17 +1,10 @@
-using System.ComponentModel.Design.Serialization;
-using System.Reflection.Metadata;
-using static aed_trabalho.Início;
-
-namespace aed_trabalho
+namespace cadastronovo
 {
-    public partial class Início : Form
+    public partial class Form1 : Form
     {
-
-        public List<account> acc = new List<account>();
-        public Início()
+        public Form1()
         {
             InitializeComponent();
-          
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -19,31 +12,10 @@ namespace aed_trabalho
 
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
-
-        private void toolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void toolStripComboBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void toolStripMenuItem2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void backgroundWorker2_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
-        {
-
-        }
-
 
         private void label2_Click(object sender, EventArgs e)
         {
@@ -55,109 +27,38 @@ namespace aed_trabalho
 
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        public void button3_Click(object sender, EventArgs e)
         {
 
-        }
+            Usuario usuario = new Usuario();
 
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
+            string usuarionome = txtusuario.Text;
+            string senha1 = txtsenha.Text;
+
+
+            usuario.username = usuarionome;
+            usuario.senha = senha1;
+
+
+
+            using (StreamWriter sw = new StreamWriter("arquivo.txt", true))
             {
-                if (!string.IsNullOrEmpty(txtUsuario.Text) && !string.IsNullOrEmpty(txtSenha.Text))
-                {
-                    if (UserExists(txtUsuario.Text))
-                    {
-                        MessageBox.Show("Usuário já existe. Escolha outro nome de usuário.");
-                        txtUsuario.Focus();
-                        return;
-                    }
-
-                    account Acc = new account();
-                    Acc.name = txtUsuario.Text;
-                    Acc.password = txtSenha.Text;
-
-                    acc.Add(Acc);
-
-                   
-
-                    var menu = new Menu();
-                    menu.Show();
-
-                    this.Visible = false;
-                }
-                else 
-                {
-                    MessageBox.Show("Preencha todas as informações!");
-                    txtUsuario.Focus();
-                    txtSenha.Text = "";
-                }
+                sw.WriteLine($"{usuario.username}, {usuario.senha}");
             }
-
-          
-            catch (Exception ex)
-            {
-                MessageBox.Show("Erro ao processar: " + ex.Message);
-            }
-        }
-
-        private bool UserExists(string text)
-        {
-            for (int i = 0; i < acc.Count; i++)
-            {
-                if (acc[i].name == text)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-       
-
-
-        public class account
-        {
-            public string name = "";
-            public string password = "";
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void progressBar2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
 
         }
     }
+}
+
+public class Usuario
+{
+    public string username { get; set; }
+    public string senha { get; set; }
+
 }
